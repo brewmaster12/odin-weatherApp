e8f48cd3c32b281e308e527ad4c9c35a44f16c04
Sean <seanywg@gmail.com>
2026-07-03 18:27:10 +0100

    write function to hit API

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const baseUrl = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+const key = "H9EZUSFRZVGDKWCKR9TMKAYNK";
+let weatherLocation = "dublin";
+
+async function getWeather() {
+    try {
+        const response = await fetch(baseUrl + weatherLocation + "?key=" + key);
+        const weather = await response.json();
+        console.log(weather);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getWeather();
+
 
